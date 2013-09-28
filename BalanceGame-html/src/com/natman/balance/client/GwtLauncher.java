69bdbbfa1e3128e4ad127405db31a6e6c4eeab6c
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-09-27 18:56:08 -0600

    Added the LibGDX game projects.

---
+++ BalanceGame-html/src/com/natman/balance/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.natman.balance.client;
+
+import com.natman.balance.BalanceGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new BalanceGame();
+	}
+}
